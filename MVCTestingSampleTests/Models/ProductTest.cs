417d113b36621abca94cfe2ba3a8e8cb493ad3c2
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-05-20 17:38:29 -0700

    Added property and static method test

---
+++ MVCTestingSampleTests/Models/ProductTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCTestingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTestingSampleTests.Models
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void Name_SetToNull_ThrowArgumentNullException()
+        {
+            Product p = new Product();
+
+            Assert.ThrowsException<ArgumentNullException>
+                (
+                    () => p.Name = null
+                );
+        }
+
+
+    }
+}
